ecad980dd5100b4486750fe2645b42d977d81cf5
AsteroidOrangeJuice <asteroidorangejuice@gmail.com>
2026-05-27 06:48:42 -0400

    Expose separate timestamp sensor for last updated

--- custom_components/vestaboard/sensor.py
+++ custom_components/vestaboard/sensor.py
@@ -1,5 +1,5 @@
 from homeassistant.core import callback
-from homeassistant.components.sensor import SensorEntity
+from homeassistant.components.sensor import SensorEntity, SensorDeviceClass
 from homeassistant.helpers.update_coordinator import (
     CoordinatorEntity
 )
@@ -20,7 +20,6 @@ class VestaboardLineEntity(CoordinatorEntity, SensorEntity):
     @property
     def extra_state_attributes(self):
         return {
-            "last_updated": self.coordinator.last_message_update,
             "current_message": self.coordinator.data,
         }
 
@@ -30,7 +29,26 @@ class VestaboardLineEntity(CoordinatorEntity, SensorEntity):
         self.async_write_ha_state()
 
 
+class VestaboardLastUpdatedEntity(CoordinatorEntity, SensorEntity):
+    _attr_has_entity_name = True
+    _attr_device_class = SensorDeviceClass.TIMESTAMP
+
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+
+    @property
+    def name(self):
+        return "Vestaboard Last Updated"
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        self._attr_native_value = self.coordinator.last_message_update
+        self.async_write_ha_state()
+
+
 async def async_setup_entry(hass, config, async_add_entities):
+    coordinator = hass.data[DOMAIN][config.entry_id]['coordinator']
     async_add_entities(
-        [VestaboardLineEntity(hass.data[DOMAIN][config.entry_id]['coordinator'], line) for line in range(6)]
+        [VestaboardLineEntity(coordinator, line) for line in range(6)]
+        + [VestaboardLastUpdatedEntity(coordinator)]
     )
